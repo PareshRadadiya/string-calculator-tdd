e67412aab3de6f910f4aba55a4485060083f1ea0
Paresh Radadiya <PareshRadadiya@hotmail.com>
2024-07-09 10:28:19 +0530

    test: single number & two number sum handling

--- index.spec.js
+++ index.spec.js
@@ -3,3 +3,11 @@ import { add } from './index';
 test('empty string returns 0', () => {
   expect(add('')).toBe(0);
 });
+
+test('single number returns the number', () => {
+  expect(add('1')).toBe(1);
+});
+
+test('two numbers returns the sum', () => {
+  expect(add('1,2')).toBe(3);
+});
