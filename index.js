f660af9ce8051b2f82e848717ace0dfceff14d0f
Paresh Radadiya <PareshRadadiya@hotmail.com>
2024-07-09 10:34:05 +0530

    fix: single number & two number sum handling

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ export function add(numbers) {
   if (!numbers) {
     return 0;
   }
-
+  const numbersArray = numbers.split(',');
+  return numbersArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
